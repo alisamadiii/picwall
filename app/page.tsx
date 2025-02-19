9931e390d474deee9c6de2435988a63de8be58ae
Ali Samadi <alirs.dev@gmail.com>
2025-02-18 20:40:08 -0800

    starting a new saas project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center dark:bg-neutral-950">
-      Build
+      PICWALL.art
     </div>
   );
 }
